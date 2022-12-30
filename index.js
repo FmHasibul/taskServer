7ff1a2c3eba0621c72bab34110beb0f44f6c2c40
FmHasibul <fmhhasan@gmail.com>
2022-12-30 19:53:27 +0600

    all api done

--- index.js
+++ index.js
@@ -1,77 +1,67 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
+
 const uri = process.env.DB_URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         const TaskCollections = client.db('Taskapp').collection('taskCollection')
         const UserCollections = client.db('Taskapp').collection('users')
-        const CompeletedTasks = client.db('Taskapp').collection('doneTasks')
+
 
         app.post('/addTask', async (req, res) => {
             const task = req.body;
-            console.log(task);
             const result = await TaskCollections.insertOne(task);
-
             res.send(result);
-
-            // Task add API 
-            app.get('/allTask', async (req, res) => {
-                const query = req.query
-                const filter = {
-                    email: query.email
-                }
-                console.log(filter)
-                const cursor = await AllTask.find(query).toArray()
-                res.send(cursor)
-            })
-
-            // Task delete 
-            app.delete('/allTask:id([0-9a-fA-F]{24})', async (req, res) => {
-                const id = req.params.id;
-                // console.log(id);
-                const query = { _id: ObjectId(id) }
-                const result = await TaskCollections.deleteOne(query)
-                res.send(result)
-            });
-            app.get('/doneTask', async (req, res) => {
-                const query = req.query
-                const filter = {
-                    email: query.email
-                }
-                console.log(filter)
-                const cursor = await AllTask.find(query).toArray()
-                res.send(cursor)
-            })
-            app.post('/doneTask', async (req, res) => {
-                const compeletedTask = req.body
-                console.log(filter)
-                const result = await compeletedTasks.insertOne(compeletedTask)
-                res.send(result)
-            })
-            app.post('/users', async (req, res) => {
-                const user = req.body
-                const result = await UserCollections.insertOne(user)
-                res.send(result)
-            });
-
-
-            app.get('/user', async (req, res) => {
-                const email = req.query.email;
-                // const decodedEmail = req.decoded.email;
-
-                const query = { email: email }
-                const user = await UserCollections.findOne(query);
-                res.send(user);
-            });
         });
+
+        app.get('/allTask', async (req, res) => {
+            const email = req.query.email
+            console.log(email);
+            const query = { user: email };
+            const cursor = await TaskCollections.find(query).toArray();
+            res.send(cursor)
+        });
+        app.put('/allTask/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const taskComplete = req.body.taskComplete
+            const query = { _id: ObjectId(id) }
+            console.log(query);
+            const updatedDoc = {
+                $set: {
+                    taskComplete: taskComplete
+                }
+            }
+            const result = await TaskCollections.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+        // Task delete 
+        app.delete("/allTask/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: ObjectId(id) };
+            const result = await TaskCollections.deleteOne(filter);
+            res.send(result);
+        });
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await UserCollections.insertOne(user)
+            res.send(result)
+        });
+
+
+
+
 
     }
     finally {
@@ -83,6 +73,7 @@ run().catch(err => { console.log(err) })
 
 
 app.get('/', (req, res) => {
+    console.log(req);
     res.send('task manager server')
 })
 app.listen(port, () => {
